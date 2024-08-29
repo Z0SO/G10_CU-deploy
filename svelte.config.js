2b02d3caebe0c3e12b55d807d1470c57b4952902
Z0SO <lautarorobales31@gmail.com>
2024-08-28 23:07:19 -0300

    Fetch: vercel en prod for verce

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   // Consult the docs to learn more about preprocessors
+//   // https://kit.svelte.dev/docs/integrations#preprocessors
+//   preprocess: preprocess(),
+
+//   kit: {
+//     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapters
+//     // for a full list of supported environments
+//     adapter: adapter(),
+
+//     // hydrate the <div id="svelte"> element in src/app.html
+//     // target is no longer used in recent versions, so it should be removed
+//   }
+// };
+
+// export default config;
+
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +29,10 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapters
-    // for a full list of supported environments
-    adapter: adapter(),
+    // Use the Vercel adapter
+    adapter: adapter({
+      // Options for the Vercel adapter can be set here if needed
+    }),
 
     // hydrate the <div id="svelte"> element in src/app.html
     // target is no longer used in recent versions, so it should be removed
@@ -18,3 +40,5 @@ const config = {
 };
 
 export default config;
+
+
